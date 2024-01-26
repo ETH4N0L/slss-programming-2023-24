7b2317f182309fd5ac85283410d21eca8b5cd4cc
Ethan CHeng <ec000321@sd38.bc.ca>
2024-01-26 14:32:25 -0800

    New

---
+++ Notes/spotify_data.py
@@ -0,0 +1,53 @@
+# Spotify data
+# Author: Ethan
+# Date: Jan 16
+import csv
+
+
+# Create a function that seraches through the data
+# Finds all songs from a given artist
+
+def find_all_songs(artist: str) -> list:
+    """Searches through a set of data and 
+    returns all songs found from a given artist"""
+# Open the file
+    with open("./spotify.csv") as f:
+        # ----- Search for all Drake Songs
+        # ----- Use linear search
+        # Create a csv reader object
+        csv_reader = csv.DictReader(f)
+
+        # Make a list to store all Drake songs
+        songs = []
+
+        # Create a counter to store the current line number
+        line_num = 0
+
+        # For every line in the file
+        for line in csv_reader:
+            # If it's the first line, print out all the headings
+            # if line_num == 0:
+                # print("The columns are: ")
+            
+                # Print on one line
+                # print(", ".join(line))
+                
+                # Print one on every line
+                # for item in line:
+                #     print(item)
+
+                line_num += 1
+        else:
+                # If the artist for this song is Drake
+                # Store the song info in the list
+                # ---- artist, song title, danceability
+                if artist.lower() in line['artist'].lower():
+                    songs.append((
+                        line['artist'],
+                        line['song_title'],
+                        line['danceability']
+                        ))
+                    
+        return songs
+
+drake_songs = find_all_songs("drake")
